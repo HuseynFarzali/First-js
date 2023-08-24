1dc853fa15b496fd376dc4fc828051a7c2681bfe
HuseynFarzali <huseynyf@code.edu.az>
2023-08-24 19:14:15 +0400

    First js

---
+++ main.js
@@ -0,0 +1,11 @@
+// let message = prompt("Enter the name: ");
+
+// while (message !== "exit") {
+//     console.log(message);
+
+//     message = prompt("Enter the name: ");
+// }
+
+for (let message = prompt("Enter the name: "); message !== "exit"; message = prompt("Enter the name: ")) {
+    console.log(message);
+}
